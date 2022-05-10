155f78708aa9cf7a5267998a040da1e1894c87dc
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-05-10 11:56:25 -0400

    Add `bin/codeownership for_file` CLI to power extension (#3)
    * renest test
    
    * wip
    
    * Add CLI for getting ownership for a file
    
    * bump version
    
    * type check

--- spec/lib/code_ownership/cli_spec.rb
+++ spec/lib/code_ownership/cli_spec.rb
@@ -1,26 +1,106 @@
 RSpec.describe CodeOwnership::Cli do
   subject { CodeOwnership::Cli.run!(argv) }
 
-  let(:argv) { ['validate'] }
+  describe 'validate' do
+    let(:argv) { ['validate'] }
 
-  before do
-    write_file('config/code_ownership.yml', <<~YML)
-      owned_globs:
-        - 'app/**/*.rb'
-    YML
+    before do
+      write_file('config/code_ownership.yml', <<~YML)
+        owned_globs:
+          - 'app/**/*.rb'
+      YML
 
-    write_file('app/services/my_file.rb')
-    write_file('frontend/javascripts/my_file.jsx')
+      write_file('app/services/my_file.rb')
+      write_file('frontend/javascripts/my_file.jsx')
+    end
+
+    context 'when run without arguments' do
+      it 'runs validations with the right defaults' do
+        expect(CodeOwnership).to receive(:validate!) do |args| # rubocop:disable RSpec/MessageSpies
+          expect(args[:autocorrect]).to eq true
+          expect(args[:stage_changes]).to eq true
+          expect(args[:files]).to match_array(['app/services/my_file.rb'])
+        end
+        subject
+      end
+    end
+
   end
 
-  context 'when run without arguments' do
-    it 'runs validations with the right defaults' do
-      expect(CodeOwnership).to receive(:validate!) do |args| # rubocop:disable RSpec/MessageSpies
-        expect(args[:autocorrect]).to eq true
-        expect(args[:stage_changes]).to eq true
-        expect(args[:files]).to match_array(['app/services/my_file.rb'])
+  describe 'for_file' do
+    before do
+      write_file('config/code_ownership.yml', <<~YML)
+        owned_globs:
+          - 'app/**/*.rb'
+      YML
+
+      write_file('app/services/my_file.rb')
+      write_file('config/teams/my_team.yml', <<~YML)
+        name: My Team
+        owned_globs: 
+          - 'app/**/*.rb'
+      YML
+    end
+
+    context 'when run with no flags' do
+      context 'when run with one file' do
+        let(:argv) { ['for_file', 'app/services/my_file.rb'] }
+        
+        it 'outputs the team info in human readable format' do
+          expect(CodeOwnership::Cli).to receive(:puts).with(<<~MSG)
+            Team: My Team
+            Team YML: config/teams/my_team.yml
+          MSG
+          subject
+        end
       end
-      subject
+
+      context 'when run with no files' do
+        let(:argv) { ['for_file'] }
+        
+        it 'outputs the team info in human readable format' do
+          expect { subject }.to raise_error "Please pass in one file. Use `bin/codeownership for_file --help` for more info"
+        end
+      end
+
+      context 'when run with multiple files' do
+        let(:argv) { ['for_file', 'app/services/my_file.rb', 'app/services/my_file2.rb'] }
+        
+        it 'outputs the team info in human readable format' do
+          expect { subject }.to raise_error "Please pass in one file. Use `bin/codeownership for_file --help` for more info"
+        end
+      end
+    end
+
+    context 'when run with --json' do
+      let(:argv) { ['for_file', '--json', 'app/services/my_file.rb'] }
+
+      context 'when run with one file' do
+        it 'outputs JSONified information to the console' do
+          json = {
+            team_name: 'My Team',
+            team_yml: 'config/teams/my_team.yml'
+          }
+          expect(CodeOwnership::Cli).to receive(:puts).with(json.to_json)
+          subject
+        end
+      end
+
+      context 'when run with no files' do
+        let(:argv) { ['for_file', '--json'] }
+        
+        it 'outputs the team info in human readable format' do
+          expect { subject }.to raise_error "Please pass in one file. Use `bin/codeownership for_file --help` for more info"
+        end
+      end
+
+      context 'when run with multiple files' do
+        let(:argv) { ['for_file', 'app/services/my_file.rb', 'app/services/my_file2.rb'] }
+        
+        it 'outputs the team info in human readable format' do
+          expect { subject }.to raise_error "Please pass in one file. Use `bin/codeownership for_file --help` for more info"
+        end
+      end
     end
   end
 end
