fa65780b1744bb1e98c6ca336f4cd54851c3129a
Perry Hertler <perry@hertler.org>
2025-02-24 18:58:54 -0600

    referencing codeowners

--- ext/libcodeowners/src/lib.rs
+++ ext/libcodeowners/src/lib.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use codeowners::runner::{self, RunConfig};
 use magnus::{function, prelude::*, Error, Ruby, Value};
 use serde::{Deserialize, Serialize};
 use serde_magnus::serialize;
@@ -10,17 +13,41 @@ fn for_team(team: String) -> Result<Value, Error> {
     let serialized: Value = serialize(&result)?;
     Ok(serialized)
 }
+
+fn generate_and_validate(files: Vec<String>) -> Result<Value, Error> {
+    let run_config = build_run_config();
+    let result = runner::generate_and_validate(&run_config, files);
+    let serialized: Value = serialize(&result)?;
+    Ok(serialized)
+}
+
+fn validate(files: Vec<String>) -> Result<Value, Error> {
+    let run_config = build_run_config();
+    let result = runner::validate(&run_config, files);
+    let serialized: Value = serialize(&result)?;
+    Ok(serialized)
+}
+
+fn build_run_config() -> RunConfig {
+    RunConfig {
+        project_root: PathBuf::from("."),
+        codeowners_file_path: PathBuf::from("./github/CODEOWNERS"),
+        config_path: PathBuf::from("./config/code_ownership.yml"),
+        no_cache: false,
+    }
+}
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CodeOwnersResult {
     pub output: Vec<String>,
     pub success: bool,
 }
 
-
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
-    let module = ruby.define_module("FastCodeowners")?;
+    let module = ruby.define_module("RustCodeOwners")?;
     module.define_singleton_method("for_team", function!(for_team, 1))?;
+    module.define_singleton_method("generate_and_validate", function!(generate_and_validate, 1))?;
+    module.define_singleton_method("validate", function!(validate, 1))?;
 
     Ok(())
 }
