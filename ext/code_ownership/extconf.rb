b2070b856d66943dce988ba780c2e39a68679a75
Joseph Sak <joseph.sak@gusto.com>
2026-01-22 18:46:01 -0700

    Build versioned native extension paths for Ruby 3.2-4.0
    Install the compiled extension into lib/code_ownership/<major.minor>/ so cross-gem
    can package multiple Ruby minors into a single platform gem without overwriting.

--- ext/code_ownership/extconf.rb
+++ ext/code_ownership/extconf.rb
@@ -3,7 +3,8 @@
 require 'mkmf'
 require 'rb_sys/mkmf'
 
-create_rust_makefile('code_ownership/code_ownership') do |ext|
+ruby_minor = RUBY_VERSION[/\d+\.\d+/]
+create_rust_makefile("code_ownership/#{ruby_minor}/code_ownership") do |ext|
   ext.extra_cargo_args += ['--crate-type', 'cdylib']
   ext.extra_cargo_args += ['--package', 'code_ownership']
 end
